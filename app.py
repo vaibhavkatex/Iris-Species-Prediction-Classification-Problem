95a4dbf76ddaf3e4ede07a52d3755109ca51fc82
Vaibhav Kate <vaibhavkate1301@gmail.com>
2026-08-01 23:34:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,39 +2,33 @@ import streamlit as st
 import joblib
 import numpy as np
 
-# Load Model and Scaler
-model = joblib.load("model.pkl")
+# Load model and scaler
+model = joblib.load("rf_tuned_model.pkl")
 scaler = joblib.load("scaler.pkl")
 
-st.set_page_config(page_title="Iris Flower Prediction", page_icon="🌸")
-
+# Page title
 st.title("🌸 Iris Flower Prediction")
-st.write("Predict the species of an Iris flower using a KNN Machine Learning model.")
 
-st.sidebar.header("Input Features")
+st.write("Enter the flower measurements and click Predict.")
 
-sepal_length = st.sidebar.slider("Sepal Length (cm)", 4.0, 8.0, 5.1)
-sepal_width = st.sidebar.slider("Sepal Width (cm)", 2.0, 5.0, 3.5)
-petal_length = st.sidebar.slider("Petal Length (cm)", 1.0, 7.0, 1.4)
-petal_width = st.sidebar.slider("Petal Width (cm)", 0.1, 3.0, 0.2)
+# Input fields
+sepal_length = st.number_input("Sepal Length (cm)", min_value=4.0, max_value=8.0, value=5.1)
+sepal_width = st.number_input("Sepal Width (cm)", min_value=2.0, max_value=5.0, value=3.5)
+petal_length = st.number_input("Petal Length (cm)", min_value=1.0, max_value=7.0, value=1.4)
+petal_width = st.number_input("Petal Width (cm)", min_value=0.1, max_value=3.0, value=0.2)
 
-features = np.array([[sepal_length,
+# Predict button
+if st.button("Predict"):
+
+    data = np.array([[sepal_length,
                       sepal_width,
                       petal_length,
                       petal_width]])
 
-scaled_features = scaler.transform(features)
+    # Scale data
+    data = scaler.transform(data)
 
-prediction = model.predict(scaled_features)[0]
+    # Prediction
+    prediction = model.predict(data)[0]
 
-species = {
-    0: "🌼 Iris-setosa",
-    1: "🌺 Iris-versicolor",
-    2: "🌹 Iris-virginica"
-}
-
-if st.button("Predict"):
-    st.success(f"Predicted Species: **{species[prediction]}**")
-
-st.markdown("---")
-st.write("Model: K-Nearest Neighbors (KNN)")
+    st.success(f"Predicted Species: {prediction}")
